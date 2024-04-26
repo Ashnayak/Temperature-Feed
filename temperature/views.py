21e9aacb389ec82f37c43b1379d84bf089d61912
Ashwini Nayak <ashwininayak1212@gmail.com>
2024-04-25 20:49:03 -0700

    Push data to postgres and fetch data.

--- temperature/views.py
+++ temperature/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+from .models import TemperatureReading
+
+def temperature_data(request):
+    # Handle incoming request to fetch temperature data
+    # You might want to query the database and return the temperature data as JSON
+    temperature_readings = TemperatureReading.objects.all()
+    data = [{'timestamp': reading.timestamp, 'temperature': reading.temperature} for reading in temperature_readings]
+    return JsonResponse(data, safe=False)
